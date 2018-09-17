56a2f9ea83222d76e8774e9aa477689083865e70
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2018-09-17 12:42:04 -0400

    Adding|Configuring swagger for API doc

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module PierpontglobalApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Only loads a smaller set of middleware suitable for API only apps.
+    # Only loads a smaller set of middleware suitable for api only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  namespace :api do
+    namespace :v1 do
+      get '/version', to: 'base#version'
+    end
+  end
 end
