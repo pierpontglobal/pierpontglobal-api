bafcdac481090eca0350f1130706eaf71b773aa0
Hector Acosta <hector.acosta.pozo@outlook.com>
2019-01-05 11:38:46 -0400

    Added auto registration for IP from REST

---
+++ app/controllers/api/v1/admin/configuration_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Admin
+
+      # Allow the administrator to control the web site configurations
+      class ConfigurationController < Api::V1::AdminBaseController
+        def register_ip
+          config_methods = ConfigMethods.new
+          config_methods.register_ip
+          render json: { status: 'success' }, status: :ok
+        end
+      end
+    end
+  end
+end
