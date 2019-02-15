57a45708ff578171e61eeb6b5fb3858df8efb6b6
Hector Acosta <hector.acosta.pozo@outlook.com>
2019-02-15 17:53:28 -0400

    Initializing admin bid controller

---
+++ app/controllers/api/v1/admin/bid/bid_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Admin
+      module Bid
+        # Manages the bid process from the administrator perspective
+        class BidController < Api::V1::AdminBaseController
+
+          # GET: /all
+          def show_bids
+            # TODO
+          end
+
+          # GET: /
+          def show_bid
+            # TODO
+          end
+
+          # PATCH: /
+          def change_bid_status
+            # TODO
+          end
+
+          # DELETE: /
+          def delete_bid
+            # TODO
+          end
+
+          # PATCH: /
+          def notify_success
+            # TODO
+          end
+        end
+      end
+    end
+  end
+end
--- app/controllers/api/v1/user/bids/bids_controller.rb
+++ app/controllers/api/v1/user/bids/bids_controller.rb
@@ -5,15 +5,16 @@ module Api
     module User
       module Bids
         class BidsController < Api::V1::BaseController
-        skip_before_action :active_user?
+          skip_before_action :active_user?
 
-        def show
-          render json: ::Bid.where(user: @user).attach_vehicle_info, status: :ok
-        end
-
-        def delete
-        end
+          def show
+            render json: ::Bid.where(user: @user).attach_vehicle_info, status: :ok
           end
+
+          def delete
+            # bid = ::Bid.find_by(user_id: 47, id: 11)
+          end
+        end
       end
     end
   end
