0235dc4d8e4da267db3e554ff825a6cf8426f7f0
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-06-10 11:57:47 -0400

    Created Worker Handler

--- lib/worker_handler.rb
+++ lib/worker_handler.rb
@@ -8,13 +8,13 @@ module WorkerHandler
     @security_group = "'sg-0903654f2c06b4b19'"
     @task_definition = 'SidekiqWorker:50'
 
-    @worker_number = 0
+    @worker_number = Sidekiq::ProcessSet.new.size || 0
 
     Thread.new do
       while true do
         update_worker_number
         # ------------------- #
-        sleep 20
+        sleep 20Z
       end
     end
   end
