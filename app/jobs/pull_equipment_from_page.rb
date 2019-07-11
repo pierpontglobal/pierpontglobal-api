062a553ef21e8c671f0cd6102a1bcf0d169bbbcb
Daniel Pena Berroa <digrape07@gmail.com>
2019-07-11 11:41:58 -0400

    crawler changes

--- app/jobs/pull_equipment_from_page.rb
+++ app/jobs/pull_equipment_from_page.rb
@@ -23,17 +23,19 @@ class PullEquipmentFromPage
             title: vehicle[:title],
             location: vehicle[:location],
             price: vehicle[:price],
-            equipment_id: vehicle[:ur_id]
+            equipment_id: vehicle[:ur_id],
+            description: vehicle["description"],
+            serial: vehicle["serial"]
         )
-        equipment_type = HeavyVehicleType.where("TRIM(lower(name)) = ?", vehicle[:equipment_type].to_s.downcase.gsub(" ", ""))[0]
+        equipment_type = HeavyVehicleType.where("lower(name) = ?", vehicle["equipment_type"].to_s.downcase)[0]
         if !equipment_type.present?
-          equipment_type = HeavyVehicleType.create!(name: vehicle[:equipment_type])
+          equipment_type = HeavyVehicleType.create!(name: vehicle["equipment_type"])
         end
         created_vehicle.type_id = equipment_type[:id]
 
-        manufacturer = HeavyVehicleManufacturer.where("TRIM(lower(name)) = ?", vehicle[:manufacturer].to_s.downcase.gsub(" ", ""))[0]
+        manufacturer = HeavyVehicleManufacturer.where("lower(name) = ?", vehicle["manufacturer"].to_s.downcase)[0]
         if !manufacturer.present?
-          manufacturer = HeavyVehicleManufacturer.create!(name: vehicle[:manufacturer])
+          manufacturer = HeavyVehicleManufacturer.create!(name: vehicle["manufacturer"])
         end
         created_vehicle.manufacturer_id = manufacturer[:id]
 
@@ -44,7 +46,10 @@ class PullEquipmentFromPage
             main_image: vehicle[:main_image],
             title: vehicle[:title],
             location: vehicle[:location],
-            price: vehicle[:price]
+            price: vehicle[:price],
+            equipment_id: vehicle[:ur_id],
+            description: vehicle["description"],
+            serial: vehicle["serial"]
         )
       end
     end
