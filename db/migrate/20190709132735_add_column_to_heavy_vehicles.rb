7176d73db8b3dea8004ce2b2088fae5e28035a68
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-07-11 18:43:43 -0400

    Update 20190709132735_add_column_to_heavy_vehicles.rb
    Removed year from table migration

--- db/migrate/20190709132735_add_column_to_heavy_vehicles.rb
+++ db/migrate/20190709132735_add_column_to_heavy_vehicles.rb
@@ -2,6 +2,5 @@ class AddColumnToHeavyVehicles < ActiveRecord::Migration[5.2]
   def change
     add_column :heavy_vehicles, :category, :string
     add_column :heavy_vehicles, :sub_category, :string
-    add_column :heavy_vehicles, :year, :integer
   end
 end
